cace176d96ea086b64b6feaf86b8dc90ff79c467
Terdoo Mzer <mzerterdoo6@gmail.com>
2026-06-21 00:12:55 +0100

    chore(web): remove TripCreationRequest file form request file and the import statement in TripController, since I already wrote Trips creation form validation logic directly in the TripController store method. In the future, I may redo the logic in a form request classs and extract the validation unto that class

--- web/app/Http/Controllers/TripController.php
+++ web/app/Http/Controllers/TripController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\TripCreationRequest;
 use App\Models\Trip;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
